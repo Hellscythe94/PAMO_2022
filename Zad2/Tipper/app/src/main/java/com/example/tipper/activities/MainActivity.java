a79774287264aada05b02bed82a58c8010ccf0a6
Michał Wołowiec <michal.wolowiec@netguru.com>
2022-03-28 19:20:36 +0200

    Added Home screen and Kcal Calculator

---
+++ Zad2/Tipper/app/src/main/java/com/example/tipper/activities/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.tipper.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.tipper.R;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bmiButton;
+    private Button kcalButton;
+    private Button recipesButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // call superclass onCreate
+        setContentView(R.layout.activity_main); // inflate the GUI
+
+        bmiButton = (Button) findViewById(R.id.bmiButton);
+        kcalButton = (Button) findViewById(R.id.kcalButton);
+        recipesButton = (Button) findViewById(R.id.recipesButton);
+
+        bmiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity(BmiActivity.class);
+            }
+        });
+
+        kcalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity(KcalActivity.class);
+            }
+        });
+
+    }
+
+    private void launchActivity(Class activity){
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
+    }
+}
